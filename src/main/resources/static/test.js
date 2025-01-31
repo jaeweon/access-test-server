e1b4e95794f9bf7cf9934db9064d424b9380af8d
jaeweon <fhfkfk789@naver.com>
2025-01-31 21:54:09 +0900

    테스트 코드 작성

--- src/main/resources/static/test.js
+++ src/main/resources/static/test.js
@@ -2,24 +2,23 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-    vus: 2000,  // 동시 사용자 1000명
-    duration: '15s',  // 테스트 지속 시간
+    vus: 5000,
+    duration: '15s',
 };
 
 export default function () {
-    let userId = `dummy_user_${__VU}`; // 가상 유저 ID 생성
+    let userId = `dummy_user_${__VU}`;
     let payload = {
         username: userId,
         password: 'defaultPassword123',
-    };
+    };d
 
-    let res = http.post('http://127.0.0.1:9000/api/v1/auth/login', payload);
+    let res = http.post('http://access-queue-system-login-server-1/api/v1/auth/login', payload);
 
     if (res.status !== 200 && res.status !== 302) {
         console.error(`User ${userId} 로그인 실패`);
     }
 
-    // 🔹 각 요청에 무작위 지연 시간 (0~2초 사이) 적용
-    let randomDelay = Math.random() * 8;  // 0~2초 랜덤 값
+    let randomDelay = Math.random() * 8;
     sleep(randomDelay);
 }
